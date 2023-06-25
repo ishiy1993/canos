28ce62a3f062a698dcb8d47e1bcb295d66968c0f
ishiy <y.ishihara.1993@gmail.com>
2023-06-25 09:28:54 +0900

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "canos";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
